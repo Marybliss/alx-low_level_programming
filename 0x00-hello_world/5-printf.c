071c5443f931804985e3c8bbae996ed41961f450
Marybliss <marybliss47@gmail.com>
2022-06-30 22:31:33 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-/
-* main - Entry point
-* Return: Always 0 (success)
-**/
+/**
+ * main - Entry point
+ * Description: C program that prints exactly with proper grammar
+ * Return: Always 0 (Success)
+ **/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
