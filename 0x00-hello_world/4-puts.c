c519568d56be1dccdc0a053db8b8494095a5d53a
Marybliss <marybliss47@gmail.com>
2022-06-30 20:05:02 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <stdio.h>
+/
+ * main - Entry point
+ *  * Return: Always 0 (sucess)
+ *   */
+ int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
