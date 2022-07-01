4bc5b5aad84cdf644bb601bba8d0c9efa83c09a9
Marybliss <marybliss47@gmail.com>
2022-07-01 00:14:48 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include < unistd.h >
+#include <unistd.h>
 /**
  *main - Entry point
  *
@@ -6,9 +6,8 @@
  **/
 int main(void)
 {
-	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
 	write(2, str1, 59);
 	return (1);
 }
-
