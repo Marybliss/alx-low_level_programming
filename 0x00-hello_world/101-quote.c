538224fd5285eb335fd77fd5ee7fe71fdb28a8be
Marybliss <marybliss47@gmail.com>
2022-07-01 01:26:49 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,10 @@
  *main - Entry point
  *
  *Return: Always 1 (Success)
- **/
+**/
 int main(void)
 {
-	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(2, str1, 59);
 	return (1);
