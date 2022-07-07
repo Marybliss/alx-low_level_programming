f2b3de5fa4036f66b178e3dd2ff9a582450e4fdb
Marybliss <marybliss47@gmail.com>
2022-07-06 20:39:11 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,10 @@
-#include "main.h"
+#include  "main.h"
 /**
  * print_alphabet_x10 - function to print abc 10 times
  *
  * Return: 0
- */
-void
-print_alphabet_x10(void)
+ **/
+void print_alphabet_x10(void)
 {
 char c, i;
 : wq
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,4 +9,5 @@ int print_last_digit(int);
 void jack_bauer(void);
 int add(int, int);
 void print_to_98(int n);
-(
+void print_times_table(int n);
+
