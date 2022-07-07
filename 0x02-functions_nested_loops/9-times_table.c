fa62fa90d3e27de54fcec6d098e0ac91f7e3ebca
Marybliss <marybliss47@gmail.com>
2022-07-06 21:58:08 -0700

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,23 +16,22 @@ if (j == 0)
 {
 _putchar(k + '0');
 }
+
 if (k < 10 && j != 0)
 {
 _putchar(',');
 _putchar(' ');
 _putchar(' ');
 _putchar(k + '0');
-}
-else
-if (k >= 10)
+} else if (k >= 10)
 {
 _putchar(',');
 _putchar(' ');
 _putchar((k / 10) + '0');
 _putchar((k % 10) + '0');
+}
+}
 _putchar('\n');
-}
-}
 }
 }
 
