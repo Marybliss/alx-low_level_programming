c0b2627fd6e29fc0417cd51e8e724f6bc418db41
Famous Mary <106754034+Marybliss@users.noreply.github.com>
2022-07-13 03:39:21 +0100

    task '7'

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,12 +7,15 @@
 void puts_half(char *str)
 {
 int index, half;
+
 index = 0;
 while (str[index] != "\0")
 index++;
+
 half = index / 2;
 if (index % 2 == 1)
 half++;
+
 while (half < index)
 {
 _putchar(str[half]);
