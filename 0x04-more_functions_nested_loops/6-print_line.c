f4767efee554fbcb6d79c51e3239f16088575767
Famous Mary <106754034+Marybliss@users.noreply.github.com>
2022-07-13 12:09:39 +0100

    task '6'

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - prints a line of _ n long
+ * @n: length of line
+ *
+ * Return: void
+ */
+
+void print_line(int n)
+{
+int line = 0;
+while (line < n)
+{
+_putchar('');
+line++;
+}
+_putchar('\n');
+}
