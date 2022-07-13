7dce4beb28a9c969feb190867c48627ea421f89d
Famous Mary <106754034+Marybliss@users.noreply.github.com>
2022-07-13 01:38:19 +0100

    task '4'

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in stdout in reverse
+ * @s: string to print
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+int i, n;
+for ( i = 0; s[i] != '\0'; i++)
+{
+}
+for (n = i - 1; n >= 0; n--)
+{
+_putchar(s[n]);
+}
+_putchar('\n');
+}
