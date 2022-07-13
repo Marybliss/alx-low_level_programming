067553ebd052c04c446587eb63f39101de28894c
Famous Mary <106754034+Marybliss@users.noreply.github.com>
2022-07-13 02:06:59 +0100

    task '7'

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *puts_half - prints half of a string
+ *@str: string length
+ *
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+int len; // index
+int counter; //half
+for (len = 0; str[len] != '\0'; len++)
+{
+}
+counter = (len + 1) / 2;
+while (str[counter])
+{
+_putchar(str[counter]);
+counter++;
+}
+_putchar('\n');
+}
