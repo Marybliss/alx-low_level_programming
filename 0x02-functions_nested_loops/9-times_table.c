d42ee4eddbd06a92e8f866f36f94ca6cc4459647
Famous Mary <106754034+Marybliss@users.noreply.github.com>
2022-07-15 03:22:26 +0100

    Task '9'

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,44 +1,46 @@
 #include "main.h"
+
 /**
- * times_table - print the table from 0 to 9
- *
- *
+ * times_table - prints time table
  * Return: void
- *
  */
+
 void times_table(void)
 {
-int x, y, z, u, d;
 
-for (x = 0; x <= 9; x++)
+int a = 0;
+int b;
+int rep;
+while (a <= 9)
 {
-for (y = 0; y <= 9; y++)
-{
-z = x * y;
+	b = 0;
+	while (b <= 9)
+	{
+		rep = a * b;
+		if (b == 0)
+		{
+			_putchar('0' + rep);
+		}
+		else if (rep < 10)
+		{
+			_putchar(' ');
+			_putchar('0' + rep);
+		}
+		else
+		{
+			_putchar('0' + rep / 10);
+			_putchar('0' + rep % 10);
+		}
 
-if (z > 9)
-{
-u = z % 10;
-d = (z - u) / 10;
-
-_putchar(44);
-_putchar(32);
-_putchar(d + '0');
-_putchar(u + '0');
-}
-else
-{
-if (y != 0)
-{
-_putchar(44);
-_putchar(32);
-_putchar(32);
+		if (b < 9)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+		b++;
+	}
+	_putchar('\n');
+	a++;
 }
 
-_putchar(z + '0');
-}
-}
-
-_putchar('\n');
-}
 }
