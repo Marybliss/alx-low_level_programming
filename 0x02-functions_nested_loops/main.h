3d3dd558cc4b3d9be6843ee0372a88e7b8647f70
Famous Mary <106754034+Marybliss@users.noreply.github.com>
2022-07-15 02:58:43 +0100

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -11,3 +14,5 @@ int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 
+
+#endif
