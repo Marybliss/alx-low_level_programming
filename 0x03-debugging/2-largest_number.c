2f1365dbeab15381038b31e7a0d3640e5c0eb36a
Famous Mary <106754034+Marybliss@users.noreply.github.com>
2022-07-15 02:36:30 +0100

    Task '2'

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -14,19 +14,19 @@ int largest;
 
 if (a > b && a > c)
 {
-	largest = a;
+largest = a;
 }
 else if (b > c && b > a)
 {
-	largest = b;
+largest = b;
 }
 else if (c > b)
 {
-	largest = c;
+largest = c;
 }
 else
 {
-	largest = b;
+largest = b;
 }
 
 return (largest);
