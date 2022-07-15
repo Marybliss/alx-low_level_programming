1bfba96c4e3edf31e45efc51bb8177bc6b76fd9d
Famous Mary <106754034+Marybliss@users.noreply.github.com>
2022-07-15 02:08:30 +0100

    Task '3'

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,18 +2,18 @@
 
 /**
  * print_numbers - print 0 - 9
- *             only using _putchar twice
+ * only using _putchar twice
  *
  * Return: Always 0 (Success)
  */
 
 void print_numbers(void)
 {
-	int num = 0;
-
-	do {
-		_putchar(num + 48);
-		num++;
-	} while (num >= 0 && num <= 9);
-	_putchar('\n');
+int num = 0;
+do 
+{
+_putchar(num + 48);
+num++;
+} while (num >= 0 && num <= 9);
+_putchar('\n');
 }
