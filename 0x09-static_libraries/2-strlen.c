7323c17c63f4729d1024b1a851440c9aadee7dcf
Famous Mary <106754034+Marybliss@users.noreply.github.com>
2022-07-23 02:43:38 +0100

    Create 2-strlen.c

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - finds the length of the string
+ * @s: pointer for the string to be checked
+ * Return: void
+ */
+
+int _strlen(char *s)
+{
+int i = 0;
+while (s[i])
+i++;
+return (i);
+}
