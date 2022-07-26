d693e74477ff2c689e2fae84456b57db279c0ffd
Famous Mary <106754034+Marybliss@users.noreply.github.com>
2022-07-26 04:15:32 +0100

    Task '0'

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - Create an array of chars
+ * @size: size of string
+ * @c: new string
+ * Return: NULL if is 0
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int con;
+	char *parray;
+
+	if (size == 0)
+		return (NULL);
+	parray = malloc(sizeof(c) * size);
+	if (parray == 0)
+		return (NULL);
+	for (con = 0; con < size; con++)
+		parray[con] = c;
+	return (parray);
+}
