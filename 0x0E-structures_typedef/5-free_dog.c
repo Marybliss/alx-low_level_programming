a5d0afefe6752b705c44f55caa922e4b47c39769
Famous Mary <106754034+Marybliss@users.noreply.github.com>
2022-08-01 18:24:49 +0100

    Task '5'

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - frees dogs.
+ * @d: structure
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
