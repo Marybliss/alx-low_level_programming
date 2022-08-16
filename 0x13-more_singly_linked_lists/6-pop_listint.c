8a03e26cec376ce936ec3e52705232326033d608
Famous Mary <106754034+Marybliss@users.noreply.github.com>
2022-08-16 04:40:35 +0100

    Task '6'

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes the head node of
+ * a linked list
+ * @head: head of a list.
+ *
+ * Return: head node's data.
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *start;
+	int n;
+
+	if (head == NULL || *head == NULL)
+		return (0);
+	start = *head;
+	*head = start->next;
+	n = start->n;
+	free(start);
+	return (n);
+}
